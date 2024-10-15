ff64f544bb7b4a3177d366cee67772fafd602ab6
Art <97910780+artndev@users.noreply.github.com>
2024-10-15 18:11:43 +0300

    Second commit

---
+++ screen/src/contexts/Article.jsx
@@ -0,0 +1,49 @@
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Markdown from 'react-markdown'
+import { useParams } from 'react-router-dom'
+import Error from './Error'
+import "../scss/Article.scss"
+
+function Article() {
+  const params = useParams()
+  const [data, setData] = useState(null)
+  const [loaded, setLoaded] = useState(false)  
+  
+  useEffect(() => {
+    console.log(params)
+    axios.get(`http://localhost:5000/${params.sub}/${params.id}/`)
+     .then(res => {
+        if (!res.data)
+            return
+
+        setData(res.data)
+        setLoaded(true)
+        console.log(params.art.replaceAll("%20", ""))
+     }) 
+     .catch(err => console.log(err))
+  }, [params])
+
+  return <>
+    {
+        loaded
+        ? <>
+         <div className="flex">
+            <h2 className='title mb50'>
+              {params.art.replaceAll("%20", "")}
+            </h2>
+            <div className='contents'>
+              <Markdown>
+                {
+                  JSON.parse(data.contents)[params.art.replaceAll("%20", "")]
+                }
+              </Markdown>
+            </div>
+         </div>
+        </>
+        : <Error />
+    }
+  </>
+}
+
+export default Article
---
+++ screen/src/contexts/Subject.jsx
@@ -0,0 +1,61 @@
+import React, { useEffect, useState } from 'react'
+import axios from "axios"
+import { useParams } from 'react-router-dom'
+import Error from './Error'
+import "../scss/Subject.scss"
+
+export default function Subject() {
+  const params = useParams()
+  const [data, setData] = useState(null)
+  const [loaded, setLoaded] = useState(false)  
+
+  useEffect(() => {
+    axios.get(`http://localhost:5000/${params.sub}/`)
+        .then(res => {
+            if (!res.data)
+                return
+
+            setData(res.data)
+            setLoaded(true)
+        }) 
+        .catch(err => console.log(err))
+  }, [params])
+
+  return <>
+    {
+        loaded
+        ? <>
+         <div className="flex">
+            <h2 className='title'>
+                Aurora
+            </h2>
+            <div className='grid'>
+                {
+                    data.map((val, i) => {
+                        return <div key={i} className="row">
+                            <h4>
+                                {val.title}
+                            </h4>
+                            <ul className="row__list">
+                                {
+                                    Object
+                                      .keys(JSON.parse(val.contents))
+                                      .map((val2, i2) => {
+                                         return <a href={`/${params.sub}/${val.id}/${val2}`}>
+                                            <li key={i2}>
+                                                {val2}
+                                            </li>
+                                        </a>
+                                       })
+                                }
+                            </ul>
+                        </div>
+                    })
+                }
+            </div>
+         </div>
+        </>
+        : <Error />
+    }
+  </>
+}
